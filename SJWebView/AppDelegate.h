e3175c024c9c650039ae589c197e9795c5f49001
Donald <donaldgucb@gmail.com>
2015-07-01 11:15:02 +0800

    双金项目
    双金项目第一个版本

---
+++ SJWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJWebView
+//
+//  Created by Donald on 15/7/1.
+//  Copyright (c) 2015年 www.Funboo.com.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
